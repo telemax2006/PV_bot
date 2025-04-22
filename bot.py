f03131382d7e360fd645d1dc76d53c112fd330e9
telemax2006 <KUCHIRKA.WSINRS@GMAIL.COM>
2025-04-22 21:30:17 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,16 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hello from Render!")
+
+def main():
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
